9e51115297757724f750a14da591eda403348654
Arthur Buldauskas <whinemore@gmail.com>
2011-04-30 13:54:35 -0400

    - Started to separate collision handlers from other code...

---
+++ physics/collision.cpp
@@ -0,0 +1,82 @@
+#include "collision.h"
+#include "bullets.h"
+
+using namespace collision;
+
+
+cpBool Begin::BulletWorld(BEGINVARS)
+{
+	CP_ARBITER_GET_SHAPES(arb, a, b);
+
+	Bullet *bullet = (Bullet*)b->data;
+	//mark the bullet object as dead
+	bullet->status = Bullet::Status::Dead;
+
+	return cpTrue;
+}
+
+cpBool Begin::ExplosiveWorld(BEGINVARS)
+{
+	CP_ARBITER_GET_SHAPES(arb, a, b);
+
+	Explosive *explosive = (Explosive*)a->data;
+
+	explosive->Explode();
+
+	return cpTrue;
+}
+
+cpBool Begin::ExplosionObject(BEGINVARS)
+{
+	CP_ARBITER_GET_SHAPES(arb, a, b);
+	Explosive *explosive = (Explosive*)a->data;
+	GameObject *object = (GameObject*)b->data;
+
+	glm::vec3 p_obj = object->GetStatus().pos;
+	glm::vec3 p_aoa = explosive->pos;
+	
+	float dx = p_aoa.x - p_obj.x;
+	float dy = p_aoa.y - p_obj.y;
+	
+	cpContactPointSet cnt = cpArbiterGetContactPointSet(arb);
+	
+	cpVect bloc;
+	
+	bloc.x = cnt.points[0].point.x - p_obj.x;
+	bloc.y = cnt.points[0].point.y - p_obj.y;
+		
+	object->Impulse(glm::vec3((-dx),(-dy), 0), bloc.x, bloc.y );
+
+	explosive->status = Bullet::Status::Dead;
+
+	return cpFalse;
+}
+
+
+//Player
+cpBool Begin::PlayerWorld(BEGINVARS)
+{
+
+	return cpTrue;
+}
+
+
+
+///PreSolve handlers
+
+cpBool PreSolve::PlayerWorld(PRESOLVEVARS)
+{
+	return cpTrue;
+}
+
+
+///Separate Handlers
+
+//Player
+void Separate::PlayerWorld(SEPARATEVARS)
+{
+
+}
+
+
+
---
+++ physics/collision.h
@@ -0,0 +1,38 @@
+#ifndef COLLISION_HEADER_GUARD
+#define COLLISION_HEADER_GUARD
+
+#include "physics.h"
+
+#define BEGINVARS	cpArbiter *arb, cpSpace *space, void *data
+#define PRESOLVEVARS BEGINVARS
+#define SEPARATEVARS BEGINVARS
+
+namespace collision
+{
+	//Begin collision handlers
+	struct Begin
+	{
+		//Projectiles
+		static cpBool BulletWorld(BEGINVARS);
+		static cpBool ExplosiveWorld(BEGINVARS);
+		static cpBool ExplosionObject(BEGINVARS);
+	
+		//Player
+		static cpBool PlayerWorld(BEGINVARS);
+	};
+	
+	struct PreSolve
+	{
+		//Player
+		static cpBool PlayerWorld(PRESOLVEVARS);
+	};
+
+	struct Separate
+	{	
+		//Player
+		static void PlayerWorld(SEPARATEVARS);
+	};
+
+} //namespace collision
+
+#endif //COLLISION_HEADER_GUARD
